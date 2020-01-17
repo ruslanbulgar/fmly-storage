347d28e71f81d394332f0bd232615bdf6ba8c1de
Ruslan Bulgar <ruslan.bulgar@planetgroupint.com>
2020-01-17 11:39:31 +0200

    First Commit

---
+++ fmly-service/src/main/java/com/lyimsoft/fmlyservice/FmlyServiceApplication.java
@@ -0,0 +1,13 @@
+package com.lyimsoft.fmlyservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FmlyServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FmlyServiceApplication.class, args);
+	}
+
+}
---
+++ fmly-service/src/test/java/com/lyimsoft/fmlyservice/FmlyServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lyimsoft.fmlyservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FmlyServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
